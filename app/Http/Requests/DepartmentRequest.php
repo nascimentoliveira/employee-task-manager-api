f57f04aca129c027df88dc090ff6b43040867a86
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-08-05 15:58:32 -0300

    feat: create department request and resource

---
+++ app/Http/Requests/DepartmentRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class DepartmentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $id = $this->route('department');
+
+        $rules = [
+            'name' => 'required|string',
+        ];
+
+        if ($id !== null) {
+            $rules['name'] .= '|unique:departments,name,' . $id;
+        }
+
+        return $rules;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse(
+            ['errors' => $validator->errors()],
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        );
+
+        throw new HttpResponseException($response);
+    }
+}
