1d6db57494c8e74bb613be7044ea089c609c4c50
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-08-05 15:29:17 -0300

    feat: create migrations

---
+++ database/migrations/2023_08_01_002122_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->required();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('assignee_id')->nullable();
+            $table->dateTime('due_date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('assignee_id')->references('id')->on('employees')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
