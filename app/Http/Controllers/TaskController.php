b00ec5db71576a467d122f28545363ca1ca2cc7f
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-08-05 21:06:28 -0300

    feat: document task route with Swagger

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,24 +14,151 @@ class TaskController extends Controller
     ) {
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/tasks",
+     *     summary="Returns a list of tasks",
+     *     @OA\Response(
+     *         response=200,
+     *         description="List of tasks",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Task")),
+     *             @OA\Property(property="meta", ref="#/components/schemas/PaginationMeta"),
+     *             @OA\Property(property="links", ref="#/components/schemas/PaginationLinks")
+     *         )
+     *     ),
+     *     security={ {"bearerAuth": {}} },
+     *     tags={"Tasks"}
+     * )
+     */
     public function index()
     {
         $tasks = $this->repository->paginate();
         return TaskResource::collection($tasks);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/tasks",
+     *     summary="Create a new task",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Task object that needs to be added",
+     *         @OA\JsonContent(ref="#/components/schemas/TaskRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Task created successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Task")),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation errors",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="errors", type="object", example={"name": {"The name field is required."}})
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(ref="#/components/schemas/InternalServerError")
+     *     ),
+     *     security={ {"bearerAuth": {}} },
+     *     tags={"Tasks"}
+     * )
+     */
     public function store(TaskRequest $request)
     {
         $task = $this->repository->create($request->validated());
         return new TaskResource($task);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/tasks/{id}",
+     *     summary="Get a task by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the task",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task details",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", ref="#/components/schemas/Task")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="TAsk not found",
+     *         @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(ref="#/components/schemas/InternalServerError")
+     *     ),
+     *     security={ {"bearerAuth": {}} },
+     *     tags={"Tasks"}
+     * )
+     */
     public function show(string $id)
     {
         $task = $this->repository->findOrFail($id);
         return new TaskResource($task);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/tasks/{id}",
+     *     summary="Update a task by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the task",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Task object that needs to be updated",
+     *         @OA\JsonContent(ref="#/components/schemas/TaskRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task details",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", ref="#/components/schemas/Task")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Task not found",
+     *         @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation errors",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="errors", type="object", example={"name": {"The name field is required."}})
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(ref="#/components/schemas/InternalServerError")
+     *     ),
+     *     security={ {"bearerAuth": {}} },
+     *     tags={"Tasks"}
+     * )
+     */
     public function update(TaskRequest $request, string $id)
     {
         $task = $this->repository->findOrFail($id);
@@ -39,13 +166,39 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/tasks/{id}",
+     *     summary="Delete a task by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the Task",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Task deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Task not found",
+     *         @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(ref="#/components/schemas/InternalServerError")
+     *     ),
+     *     security={ {"bearerAuth": {}} },
+     *     tags={"Tasks"}
+     * )
+     */
     public function destroy(string $id)
     {
         $task = $this->repository->findOrFail($id);
         $task->delete();
-        return response()->json(
-            ['message' => 'Department deleted successfully!'],
-            Response::HTTP_OK,
-        );
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Task",
+ *     title="Task",
+ *     description="Task model representing a task entity.",
+ *     @OA\Property(property="id", type="integer", format="int64", description="Task ID", example="1"),
+ *     @OA\Property(property="title", type="string", description="Task title", example="Complete Project Report"),
+ *     @OA\Property(property="description", type="string", description="Task description (optional)", example="Finish writing the project report"),
+ *     @OA\Property(property="assignee_id", type="integer", format="int64", description="ID of the assigned employee", example="1"),
+ *     @OA\Property(property="due_date", type="string", format="date-time", description="Due date for the task (optional)", example="2023-08-15T10:00:00Z"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", description="Date and time when the task was created", example="2023-08-01T01:41:44.000000Z"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", description="Date and time of the last update to the task", example="2023-08-01T02:15:10.000000Z"),
+ * )
+ *
+ * @OA\Schema(
+ *     schema="TaskRequest",
+ *     title="Task Request",
+ *     description="Task model for request body",
+ *     @OA\Property(property="title", type="string", description="Task title", example="Complete Project Report"),
+ *     @OA\Property(property="description", type="string", description="Task description (optional)", example="Finish writing the project report"),
+ *     @OA\Property(property="assignee_id", type="integer", format="int64", description="ID of the assigned employee", example="1"),
+ *     @OA\Property(property="due_date", type="string", format="date-time", description="Due date for the task (optional)", example="2023-08-15T10:00:00Z"),
+ * )
+ */
+
 class Task extends Model
 {   
     protected $fillable = [
